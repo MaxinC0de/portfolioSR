9c47c955d83c94e5b9ee3308bfaea4ff65ab9567
Maxime <pro.maximebreton@gmail.com>
2025-04-28 10:27:55 +0200

    -

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return(
-        <div className="relative mt-12">
-            <p className="p-0 -mb-[8%] text-stretch text-[8px] lg:text-2xl absolute bottom-12 -right-12">2025 ©</p>
+        <div className="relative mt-24">
+            <p className="text-right text-stretch text-[8px] lg:text-2xl">2025 ©</p>
         </div>
     )
 }
