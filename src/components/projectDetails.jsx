f3e5e13455f2a0830453f350b16468192f680eb5
Maxime <pro.maximebreton@gmail.com>
2025-05-21 16:47:23 +0200

    -

--- src/components/projectDetails.jsx
+++ src/components/projectDetails.jsx
@@ -1,10 +1,17 @@
 import { motion, AnimatePresence } from "framer-motion"
+import { useState } from "react"
+import { SquareX } from "lucide-react"
 
 export default function ProjectDetails({ currentTitle }) {
     const currentSrc = currentTitle ? currentTitle.toLowerCase() : "avarin"
-    const imgClass = "rounded-2xl"
+    const [srcModal, setSrcModal] = useState(null)
+    const openImageModal = (i) => {
+        setSrcModal(true)
+        setSrcModal(`/images/${currentSrc}/${i}.png`)
+    }
+    console.log(srcModal)
     return(
-        <div className="mx-auto relative origin-top h-[60vh]">
+        <div className="mx-auto mt-[1vh] relative origin-top h-[60vh]">
             <AnimatePresence>
                 <motion.div 
                     className="overflow-y-scroll h-[100%]"
@@ -13,23 +20,26 @@ export default function ProjectDetails({ currentTitle }) {
                     transition={{ duration: 0.2 }}
                     exit={{ height: 0 }}
                 >
-                    <div className="flex flex-col gap-y-5">
-                        <img className={imgClass} src={`/images/${currentSrc}/1.png`} />
-                        <img className={imgClass} src={`/images/${currentSrc}/2.png`} />
-                        <img className={imgClass} src={`/images/${currentSrc}/3.png`} />
-                        <img className={imgClass} src={`/images/${currentSrc}/4.png`} />
-                        <img className={imgClass} src={`/images/${currentSrc}/5.png`} />
-                        <img className={imgClass} src={`/images/${currentSrc}/6.png`} />
-                    </div>
-                    
-                        
-                   
-                    
+                    <div className="flex flex-col gap-y-3">
+                        {Array.from({ length: 6 }).map((el, i) => (
+                            <img onClick={() => {openImageModal(i + 1)}} key={i} className="rounded-2xl cursor-pointer" src={`/images/${currentSrc}/${i + 1}.png`} />
+                        ))}
+                    </div>             
                 </motion.div>   
             </AnimatePresence>
            <div className="absolute bottom-0 h-36 bg-gradient-to-t from-black via-black/30 to-transparent w-full">
 
             </div>
+            {srcModal && 
+                <>
+                    <div className="fixed opacity-80 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black h-screen w-screen z-40"></div>
+                    <div className="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center p-2 border-5 border-white rounded-2xl">
+                        <img className="rounded-lg max-w-[90vw] max-h-[90vh] object-contain" src={srcModal} alt="" />
+                        <SquareX onClick={() => {setSrcModal(null)}} size={48} color="#ff0000" className="absolute right-6 top-6 cursor-pointer" />
+                    </div>
+                </>
+                
+            }
         </div>
          
     )
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,70 +1,89 @@
 import { motion } from "framer-motion"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import useStore from "./store"
 import ProjectDetails from "./projectDetails"
 import { SquarePlus } from "lucide-react"
 
 export default function Projects({ handleProjectSelect }) {
-    const isDesktop = useStore((state) => state.isDesktop)
-    const PROJECTS = [
-        { 
-            id: "1",
-            title: "AVARIN",
-            description: "MARKETING",
-            project: "avarin"
-         },
-         {
-            id: "2",
-            title: "DIDUS ART",
-            description: "ART",
-            project: "didus-art"
-         },
-         {
-            id: "3",
-            title: "LA MAISON DU SOLITAIRE",
-            description: "E-COMMERCE",
-            project: "la-maison-du-solitaire"
-         }
-    ]
-    const [focusedTitle, setFocusedTitle] = useState("AVARIN")
-    const handleFocus = (e, title) => {
-        setFocusedTitle(prev => prev === title ? title : title)
-        handleProjectSelect(title)
+  const isDesktop = useStore((state) => state.isDesktop)
+  const PROJECTS = [
+    { id: "1", title: "AVARIN", description: "MARKETING", project: "avarin" },
+    { id: "2", title: "DIDUS-ART", description: "ART", project: "didus-art" },
+    { id: "3", title: "LA-MAISON-DU-SOLITAIRE", description: "E-COMMERCE", project: "la-maison-du-solitaire" },
+  ]
+
+  useEffect(() => {
+    if (isDesktop) {
+      setFocusedTitle("AVARIN")
+      setOpenProjectId(null)
+    } else {
+      setFocusedTitle("")
     }
-    const [openProjectId, setOpenProjectId] = useState(null)
-    const toggleDetails = (id) => {
-        setOpenProjectId(prev => (prev === id ? null : id))
-    }
-    return(
-        <div className="text-[#fffef4] shadow-[5px_5px_0_0_#fff">
-            <h1 className="text-[#909090] text-[3vw] md:text-lg font-extrabold my-4 md:mt-0">NOS PROJETS</h1>
-            <ul className="flex flex-col gap-2">
-            {PROJECTS.map(({ id, title, description }, i) => (
-                <>
-                    <li key={id} className="py-3 border-t-[0.5px] border-[#909090]">
-                        <button onClick={(e) => handleFocus(e, title)} className="relative flex items-center w-full cursor-pointer">
-                            <motion.span 
-                                className="absolute bg-[#ff0000] size-4 blur-[3px] mr-3 rounded-[50%] flex-shrink-0"
-                                initial={false}
-                                animate={focusedTitle === title ? { opacity: 1, scaleX: 1 } : { opacity: 0, scaleX: 0 }}
-                                transition={{ duration: 0.2, ease: "easeInOut", delay: 0.1 }}
-                            >
-                            </motion.span>
-                            <motion.p 
-                                className={`text-neue-bold md:text-[1.11vw] ${focusedTitle === title ? "text-[#ff0000]" : "text-[#fffef4]"} text-[16px]`}
-                                animate={{ x: focusedTitle === title ? 25 : 0 }}
-                                transition={{ type: "twin", duration: 0.4, ease: "easeInOut" }}
-                            >{title}</motion.p>
-                            <div className="flex items-center ml-auto">
-                                <p className={`text-inter mr-12 md:mr-0 ${focusedTitle === title && "text-[#ff0000]"} md:text-[0.83vw] md:font-light text-[2.2vw] font-semibold`}>{description}</p>
-                                {!isDesktop && <button onClick={(e) => {e.stopPropagation(); toggleDetails(id)}} className=""><SquarePlus size={20} className={`cursor-pointer ${focusedTitle === title ? "text-[#ff0000]" : "text-white"}`} /></button>}
-                            </div>             
-                        </button>
-                    </li>
-                    {!isDesktop && openProjectId === id && <ProjectDetails />} 
-                </>
-            ))}
-            </ul>
-        </div>
-    )
+  }, [isDesktop])
+
+  const [focusedTitle, setFocusedTitle] = useState(null)
+  const [openProjectId, setOpenProjectId] = useState(null)
+
+  const handleClick = (title) => {
+    const isSame = focusedTitle === title
+    setFocusedTitle(title)
+    handleProjectSelect(isSame ? "" : title)
+  }
+
+  const handleMobileClick = (e, id, title) => {
+    const isSame = focusedTitle === title
+    e.stopPropagation()
+    setOpenProjectId((prev) => (prev === id ? null : id))
+    setFocusedTitle(isSame ? "" : title)
+  }
+
+  return (
+    <div className="text-[#fffef4]">
+      <h1 className="text-[#909090] text-[3vw] md:text-lg font-extrabold my-4 md:mt-0">NOS PROJETS</h1>
+      <ul className="flex flex-col gap-2">
+        {PROJECTS.map(({ id, title, description }) => (
+          <li key={id} className="py-3 border-t-[0.5px] border-[#909090]">
+            <button
+              onClick={() => isDesktop && handleClick(title)}
+              className="relative flex items-center w-full cursor-pointer"
+            >
+              <motion.span
+                className="absolute bg-[#ff0000] size-4 blur-[3px] mr-3 rounded-[50%] flex-shrink-0"
+                initial={false}
+                animate={focusedTitle === title ? { opacity: 1, scaleX: 1 } : { opacity: 0, scaleX: 0 }}
+                transition={{ duration: 0.2, ease: "easeInOut", delay: 0.1 }}
+              />
+              <motion.p
+                className={`text-neue-bold md:text-[1.11vw] ${focusedTitle === title ? "text-[#ff0000]" : "text-[#fffef4]"} text-[16px]`}
+                animate={{ x: focusedTitle === title ? 25 : 0 }}
+                transition={{ type: "twin", duration: 0.4, ease: "easeInOut" }}
+              >
+                {title}
+              </motion.p>
+              <div className="flex items-center ml-auto">
+                <p
+                  className={`text-inter mr-12 md:mr-0 ${
+                    focusedTitle === title ? "text-[#ff0000]" : ""
+                  } md:text-[0.83vw] md:font-light text-[2.2vw] font-semibold`}
+                >
+                  {description}
+                </p>
+                {!isDesktop && (
+                  <button onClick={(e) => handleMobileClick(e, id, title)}>
+                    <SquarePlus
+                      size={20}
+                      className={`cursor-pointer ${
+                        focusedTitle === title ? "text-[#ff0000]" : "text-white"
+                      }`}
+                    />
+                  </button>
+                )}
+              </div>
+            </button>
+            {!isDesktop && openProjectId === id && <ProjectDetails currentTitle={focusedTitle} />}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
