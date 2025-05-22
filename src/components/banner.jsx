296a3fbb9b9e59f3aae5feb80e4b5f17c04fbe0e
Maxime <pro.maximebreton@gmail.com>
2025-05-22 15:51:08 +0200

    -

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -4,7 +4,7 @@ export default function Banner() {
     const { isDesktop } = useStore()
     return(
         <div className="border-t-2 border-b-2 py-2 mb-4 border-[#909090] text-neue-bold text-2xl lg:text-8xl font-black text-center text-[#fffef4]">
-            <img src="/images/studioR.png" alt="" />
+            <img src="/images/banner.png" alt="" />
         </div>
     )
 }
