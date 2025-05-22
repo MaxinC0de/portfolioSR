d39fe88562e10e7ab051ab6bf41cec16345cdbbe
Maxime <pro.maximebreton@gmail.com>
2025-05-22 15:53:17 +0200

    -

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -3,7 +3,7 @@ import useStore from "./store"
 export default function Banner() {
     const { isDesktop } = useStore()
     return(
-        <div className="border-t-2 border-b-2 py-2 mb-4 border-[#909090] text-neue-bold text-2xl lg:text-8xl font-black text-center text-[#fffef4]">
+        <div className="border-t-2 border-b-2 py-2 mb-4 flex justify-center border-[#909090] text-neue-bold text-2xl lg:text-8xl font-black text-center text-[#fffef4]">
             <img src="/images/banner.png" alt="" />
         </div>
     )
